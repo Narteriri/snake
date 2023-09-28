4802f16c622cf0e068c2a5480e96ba4aef7c09f3
yaojie wu <1210154605@qq.com>
2023-09-28 18:03:30 +0800

    initalize pages that in frontend

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,36 +1,21 @@
 <template>
-  <div>
-    <div>bot名称:{{ bot_name }}</div>
-    <div>bot战力:{{ bot_power }}</div>
-  </div>
+
+  <nav-bar></nav-bar>
   <router-view></router-view>
 </template>
 
 <script>
-  import $ from 'jquery';
-  import { ref } from 'vue';
+
+  import 'bootstrap/dist/css/bootstrap.min.css';
+  import 'bootstrap/dist/js/bootstrap';
+  import NavBar from './components/NavBar.vue';
 
   export default {
+    components: { 
+      NavBar 
+    },
     name: "App",
     setup: () => {
-
-      let bot_name = ref("")
-      let bot_power = ref("")
-
-      $.ajax({
-        url: "http://localhost:3030/pk/getbotinfo/",
-        type: "get",
-        success: resp => {
-          bot_name.value = resp.name;
-          bot_power.value = resp.power;
-          console.log(resp)
-        }
-      })
-
-      return {
-        bot_name,
-        bot_power
-      };
     }
   }
 </script>
